b476e6dca7b7181f50511ac99aaebba6f2ede744
Moi Petitnoel <moivp4@gmail.com>
2020-02-14 06:49:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,15 +1,15 @@
 numbers = [1,2,3]
 
 def square_array(numbers)
-  counter = 1
+  counter = 0
   new_numbers = []
   #puts "length of numbers array:"
   #puts numbers.length
   while counter <= (numbers.length - 1) do 
-    puts "counter:"
-    puts counter
+    #puts "counter:"
+    #puts counter
     x = numbers[counter]*numbers[counter]
-    #puts x
+    puts x
     #puts new_numbers.push(x)
     counter += 1
   end  
